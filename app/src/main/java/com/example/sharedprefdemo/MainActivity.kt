d17f0e80e277d6a32ad535a0141bea565623191f
TrushaKheni <trushakheni.2175@gmail.com>
2023-04-18 17:33:54 +0530

    move code line in function

--- app/src/main/java/com/example/sharedprefdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedprefdemo/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.sharedprefdemo
 
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.sharedprefdemo.databinding.ActivityMainBinding
 
-@Suppress("NAME_SHADOWING")
 class MainActivity : AppCompatActivity() {
     lateinit var moBinding: ActivityMainBinding
-    private val txt=""
-    private val switch=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         moBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,35 +17,35 @@ class MainActivity : AppCompatActivity() {
 
 
         loadData()
-        moBinding.tvPrint.text = txt
-        moBinding.onOff.isChecked= switch
 
         moBinding.btnMove.setOnClickListener {
             moBinding.tvPrint.text = moBinding.etText.text
-            Log.e("print",moBinding.tvPrint.text.toString())
+            Log.e("print", moBinding.tvPrint.text.toString())
         }
 
         moBinding.btnSave.setOnClickListener {
-           saveData()
+            saveData()
         }
     }
 
     private fun saveData() {
-        val sharedPreference =  getSharedPreferences("PREFERENCE_NAME",Context.MODE_PRIVATE)
+        val sharedPreference = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
         val editor = sharedPreference.edit()
-        editor.putString("text",moBinding.tvPrint.text.toString())
-        Log.e("print",moBinding.tvPrint.text.toString())
-        editor.putBoolean("switch",moBinding.onOff.isChecked)
+        editor.putString("text", moBinding.tvPrint.text.toString())
+        Log.e("print", moBinding.tvPrint.text.toString())
+        editor.putBoolean("switch", moBinding.onOff.isChecked)
         editor.apply()
-        Toast.makeText(this,"Save data Successfully...",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Save data Successfully...", Toast.LENGTH_SHORT).show()
     }
 
-    fun loadData() {
-        val sharedPreference =  getSharedPreferences("PREFERENCE_NAME",Context.MODE_PRIVATE)
-        val txt = sharedPreference.getString("text","")
-        Log.e("save data","pref ${ txt}")
-        val switch = sharedPreference.getBoolean("switch",false)
-        Log.e("save data", "pref ${ switch}")
+    private fun loadData() {
+        val sharedPreference = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+        val txt = sharedPreference.getString("text", "")
+        Log.e("save data", "pref $txt")
+        val switch = sharedPreference.getBoolean("switch", false)
+        Log.e("save data", "pref $switch")
+        moBinding.tvPrint.text = txt
+        moBinding.onOff.isChecked = switch
 
     }
 }
